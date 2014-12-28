eb5327ee4572aa8be0954df0727eb9d5a59b6bef
Michael Uskov <uskovm@gmail.com>
2014-12-29 03:35:20 +0500

    Simple "COMMENT)))" button

--- stavklass.js
+++ stavklass.js
@@ -1,3 +1,5 @@
-$(window).load(function() {
-	alert(1);
+﻿jQuery(window).load(function() {
+	jQuery(".add_media_more_node").append("<a class='add_media_type_1_komment add_media_item' " +
+									"style='background-image: url(http://vk.com/images/icons/attach_icons.png?6); " +
+									"background-position: 3px -152px;'><nobr>КОММЕНТ)))</nobr></a>");
 });
