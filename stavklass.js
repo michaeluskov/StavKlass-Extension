5136eaf1d22a6824b32e267ead57c86390e727a9
Michael Uskov <uskovm@gmail.com>
2014-12-30 02:55:47 +0500

    Changed logic

--- stavklass.js
+++ stavklass.js
@@ -1,13 +1,14 @@
 ﻿var $  = jQuery;
 
-var KlassMutationObserver = function() {
+var KlassMutationObserver = function(buttonsManager) {
+	this.__buttonsManager = buttonsManager;
 	this.__MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 	
 	this.__callback = function(mutations, observer) {
-		console.log(mutations, observer);
+		this.__buttonsManager.handle();
 	};
 	
-	this.__observer = new this.__MutationObserver(this.__callback);
+	this.__observer = new this.__MutationObserver(this.__callback.bind(this));
 	
 	KlassMutationObserver.prototype.start = function() {
 		this.__observer.observe($('#custom_menu_cont')[0], {
@@ -18,9 +19,40 @@ var KlassMutationObserver = function() {
 	
 };
 
+var KlassButton = function() {
+	
+	this.__button = $('<a class="add_media_item"	\
+								style="background-image: url(http://vk.com/images/icons/attach_icons.png?6);  \
+								background-position: 3px 3px;"><nobr>СТАВЬ КЛАСС)))</nobr></a>');
+								
+								
+	KlassButton.prototype.getButton = function() {
+		return this.__button;
+	};
+	
+};
+
+var KlassButtonsManager = function() {
+	
+	this.__klassButtons = {};
+	
+	KlassButtonsManager.prototype.handle = function() {
+		$('.add_media_menu').each(function (num, el) {
+			num = el.id.split('_')[3];
+			if (!(num in this.__klassButtons)) {
+				this.__klassButtons[num] = new KlassButton(this);
+				$(el).find('.add_media_items').append(this.__klassButtons[num].getButton());
+			}
+		}.bind(this));
+	
+	};
+
+};
+
 
 var init = function() {
-	var observer = new KlassMutationObserver();
+	var buttonsManager = new KlassButtonsManager();
+	var observer = new KlassMutationObserver(buttonsManager);
 	observer.start();
 };
 
