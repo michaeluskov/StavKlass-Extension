aaebaaff85d9bdfba655796e9552ee215cc7b202
Michael Uskov <uskovm@gmail.com>
2014-12-29 20:22:08 +0500

    Added checking by interval and refactoring

--- stavklass.js
+++ stavklass.js
@@ -1,5 +1,23 @@
-﻿jQuery(window).load(function() {
-	jQuery(".add_media_more_node").append("<a class='add_media_type_1_komment add_media_item' " +
+﻿var lastLocation = '';
+var checkChangeIntervalID = '';
+
+var addCommentButtons = function() {
+	var postButton = jQuery("<a class='add_media_type_1_komment add_media_item' " +
 									"style='background-image: url(http://vk.com/images/icons/attach_icons.png?6); " +
 									"background-position: 3px -152px;'><nobr>КОММЕНТ)))</nobr></a>");
+	jQuery(".add_media_more_node").append(postButton);
+};
+
+var checkIfLocationChanged = function() {
+	var newLocation = document.location.pathname;
+	var isChanged = (newLocation !== lastLocation);
+	lastLocation = newLocation;
+	return isChanged;
+};
+
+jQuery(window).load(function() {
+	setInterval(function() {
+		if (checkIfLocationChanged())
+			addCommentButtons();
+	}, 500);
 });
