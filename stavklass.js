5fcf853db3dd0eae03d27e99fe9fcf2770401f32
Michael Uskov <uskovm@gmail.com>
2014-12-30 02:29:11 +0500

    Давай по-новой, Миша, все хуйня

--- stavklass.js
+++ stavklass.js
@@ -1,18 +1,29 @@
-﻿var addCommentButtons = function() {
-	jQuery(".add_media_menu").each(function(num, el) {
-		var num = el.id.split('_')[3];
-		if (!jQuery("#add_media_item_"+num+"_komment").length) {
-			var button = jQuery('<a class="add_media_item"	\
-								style="background-image: url(http://vk.com/images/icons/attach_icons.png?6);  \
-								background-position: 3px 3px;"><nobr>СМЕШНОЙ КОММЕНТ)))</nobr></a>');
-			jQuery(button).attr('id', "add_media_item_"+num+"_komment");
-			jQuery(el).find('.add_media_items').append(button);
-		}
-	});
+﻿var $  = jQuery;
+
+var KlassMutationObserver = function() {
+	this.__MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	
+	this.__callback = function(mutations, observer) {
+		console.log(mutations, observer);
+	};
+	
+	this.__observer = new this.__MutationObserver(this.__callback);
+	
+	KlassMutationObserver.prototype.start = function() {
+		this.__observer.observe($('#custom_menu_cont')[0], {
+					childList: true,
+					subtree: true
+					});
+	};
+	
+};
+
+
+var init = function() {
+	var observer = new KlassMutationObserver();
+	observer.start();
 };
 
 jQuery(window).load(function() {
-	checkChangeIntervalID = setInterval(function() {
-			addCommentButtons();
-	}, 2000);
+	init();
 });
