afe440313e095ef4b5012a48e670a4834749faad
Michael Uskov <uskovm@gmail.com>
2015-03-31 15:11:31 +0500

    Now injector waits for jQuery

--- vk-chrome-injector.js
+++ vk-chrome-injector.js
@@ -4,6 +4,13 @@ function addScript(url) {
 	(document.head||document.documentElement).appendChild(s);
 }
 
+function addScriptIfJquery(url) {
+	if (typeof jQuery === undefined)
+		setTimeout(function() {addScriptIfJquery(url), 100};
+	else
+		addScript(url);
+}
+
 function addStylesheet(url) {
 	var s = document.createElement('link');
 	s.rel = 'stylesheet';
@@ -12,7 +19,7 @@ function addStylesheet(url) {
 }
 
 addScript('jquery.js');
-addScript('autocomplete.js');
-addScript('stavklassobject.js');
-addScript('vk.js');
+addScriptIfJquery('autocomplete.js');
+addScriptIfJquery('stavklassobject.js');
+addScriptIfJquery('vk.js');
 addStylesheet('stavklass.css');
