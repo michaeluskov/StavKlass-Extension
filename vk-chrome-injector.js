d3d4611df76585cd0267cb246f30baf3264c13f3
Michael Uskov <uskovm@gmail.com>
2015-04-09 21:55:48 +0500

    Now injector waits for jQuery

--- vk-chrome-injector.js
+++ vk-chrome-injector.js
@@ -4,6 +4,44 @@ function addScript(url) {
 	(document.body||document.documentElement).appendChild(s);
 }
 
+function startWaitingForjQuery() {
+	var s = document.createElement('script');
+	s.id = 'jQuerycheckah';
+	s.innerHTML = "function checkJq() {\
+						if (typeof jQuery !== 'undefined') {\
+							jQuery(document.body).append(jQuery('<div id=\"jQueryhere\">'));\
+							jQuery('#jQuerycheckah').remove();\
+						} else {\
+							setTimeout(checkJq, 0);\
+						}\
+					}\
+					\
+					checkJq();";
+	document.head.appendChild(s);
+	hasJq = 0;
+}
+
+function isjQueryLoaded() {
+	if (hasJq == 1)
+		return hasJq;
+	var here = document.getElementById('jQueryhere');
+	if (here !== null) {
+		here.parentNode.removeChild(here);
+		hasJq = 1;
+	} else {
+		hasJq = 0;
+	}
+	return hasJq;
+}
+
+function addScriptIfjQuery(url) {
+	if (isjQueryLoaded()) {
+		addScript(url);
+	} else {
+		setTimeout(function() {addScriptIfjQuery(url)}, 0);
+	}
+}
+
 function addStylesheet(url) {
 	var s = document.createElement('link');
 	s.rel = 'stylesheet';
@@ -11,9 +49,11 @@ function addStylesheet(url) {
 	(document.head||document.documentElement).appendChild(s);
 }
 
+
 addScript('jquery.js');
-addScript('autocomplete.js');
-addScript('scrollock.js');
-addScript('stavklassobject.js');
-addScript('vk.js');
+startWaitingForjQuery();
+addScriptIfjQuery('autocomplete.js');
+addScriptIfjQuery('scrollock.js');
+addScriptIfjQuery('stavklassobject.js');
+addScriptIfjQuery('vk.js');
 addStylesheet('stavklass.css');
